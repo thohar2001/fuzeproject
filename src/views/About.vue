b35f591c22197d34c2ccd3792fa8eced92d64ec3
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-03-22 12:21:56 +0100

    About page
    Added a about page for application

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,69 @@
 <template>
-<p>Om Fuze</p>
-<div>
-  <div></div>
-  <div></div>
-</div>
-<p>Sidan använder SR API <br> 2021 Fuze</p>
+<h1 class="firsttitle">Om Fuze</h1>
+
+    <div id="container">
+        
+        <div class="programming"> <h2 class="title">Programmering</h2>
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Mehrdad Badeie</h3> 
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Tommi Ahonen</h3> 
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Thomas Haraldsson</h3> 
+        </div>
+
+        <div class="design"><h2 class="title">Design</h2>
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Elinur Hakbilen</h3> 
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Filip Alexandrow</h3> 
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Jonas Johansson</h3> 
+            <h3><img src="..\src\assets\icon-avatar-default.png" alt=""> Rita Anthony</h3> 
+        </div>
+
+        <p>Sidan använder SR API <br> &#169; Fuze</p>
+
 </template>
 
 <script>
 
+
 </script>
 
+
 <style scoped>
+
+body {
+    font-family: Verdana;
+    text-align: center;
+}
+
+h3 {
+    font-weight: 500;
+    text-align: start;
+    margin-left: 30px;
+}
+
+.programming {
+    border-radius: 40px;
+    background-color: rgb(184, 184, 184);
+}
+
+.design {
+    border-radius: 40px;
+    background-color: rgb(184, 184, 184);
+}
+
+.title {
+    font-weight: bold;
+}
+
+.firsttitle {
+    font-weight: bolder;
+}
+
+img {
+    width: 50px;
+    height: 50px;
+}
+
+p {
+    margin-top: 20px;
+}
 
 </style>
