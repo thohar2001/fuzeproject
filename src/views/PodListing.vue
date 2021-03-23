9bba2589f07a160bb540a74d071998dedda2af1f
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-03-23 15:05:24 +0100

    Add more watchers to PodListing.vue

--- src/views/PodListing.vue
+++ src/views/PodListing.vue
@@ -1,113 +1,142 @@
 <template>
+  <label
+    >Välj tid:
+    <select ref="podLength">
+      <option value="30">30-40</option>
+      <option value="40">40-50</option>
+      <option value="50">50-60</option>
+      <option value="60">60-70</option>
+    </select>
+  </label>
 
-  <label>Välj tid:
-  <select ref="podLength">
-    <option value="30">30-40</option>
-    <option value="40">40-50</option>
-    <option value="50">50-60</option>
-    <option value="60">60-70</option>
-  </select>
- </label>
-
-  <label>Välj kategori:  
-  <select ref="podCategory" @change="categoryDropdownChanged">
-    <option v-for="(category, index) in categoryList" :key="index" :value="category.id">{{category.name}}</option>
-  </select>
+  <label
+    >Välj kategori:
+    <select ref="podCategory" @change="categoryDropdownChanged">
+      <option
+        v-for="(category, index) in categoryList"
+        :key="index"
+        :value="category.id"
+      >
+        {{ category.name }}
+      </option>
+    </select>
   </label>
 
   <label>
-  Välj program:
-  <select ref="podProgram" @change="programDropdownChanged">
-    <option v-for="(program, index) in programListFilteredByCategory" :key="index" :value="program.id">{{program.name}}</option>
-  </select>
+    Välj program:
+    <select ref="podProgram" @change="programDropdownChanged">
+      <option
+        v-for="(program, index) in programListFilteredByCategory"
+        :key="index"
+        :value="program.id"
+      >
+        {{ program.name }}
+      </option>
+    </select>
   </label>
-
 
   <!-- <p v-for="(podcast, index) in podcastList" :key="index" :value="program.id">{{podcast.name}}</p> -->
   <!-- <PodCircle></PodCircle> -->
-
 </template>
 
 <script>
-import { getAllPrograms, getProgramCategories } from '../lib/data.js'
+import { getAllPrograms, getProgramCategories } from "../lib/data.js";
 
 export default {
   name: "PodListing",
-  
+
   data() {
     return {
-      monthlist: ["January", "Februari", "March", "April", "May", "June", "July", "August", "September", "Oktober", "November", "December"],
+      monthlist: [
+        "January",
+        "Februari",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "Oktober",
+        "November",
+        "December",
+      ],
       categoryList: [],
-      programList: [],
+      programListAll: [],
       programListFilteredByCategory: [],
       selectedCategoryId: null,
-      selectedProgramId: null
+      selectedProgramId: null,
+      timeIntervalMin: null,
+      timeIntervalMax: null,
     };
   },
   async mounted() {
-    this.categoryList = await getProgramCategories()
-    this.programList = await getAllPrograms()
-    this.programListFilteredByCategory = this.programList.slice()
+    this.categoryList = await getProgramCategories();
+    this.programListAll = await getAllPrograms();
 
-    // Get initial values for categoryid from dropdown lists on html page
-    this.selectedCategoryId = this.$refs.podCategory.value
-
-    console.log("Original selectedCategoryId=" + this.selectedCategoryId)
-    console.log("Original selectedProgramId=" + this.selectedProgramId + "")
+    // TODO: remove this and create a watcher for this array
+    var event = new Event("change");
+    this.$refs.podCategory.dispatchEvent(event);
   },
   methods: {
     filterProgramsByCategory(categoryId) {
-      this.programListFilteredByCategory = this.programList.slice()
-      this.programListFilteredByCategory = this.programList.filter(program => { 
-        
-        if (program.programcategory) {
-          return program.programcategory.id == categoryId
-        } else {
-          return false
+
+      this.programListFilteredByCategory = this.programListAll.filter(
+        (program) => {
+          if (program.programcategory) {
+            return program.programcategory.id == categoryId;
+          } else {
+            return false;
+          }
         }
-
-      })
-
-      console.log("Filtering categories by " + categoryId + " (result length " + this.programListFilteredByCategory.length + " items)")
+      );
     },
 
     categoryDropdownChanged(evt) {
       this.selectedCategoryId = evt.target.value;
     },
-    
+
     programDropdownChanged(evt) {
       this.selectedProgramId = evt.target.value;
-    }
+    },
   },
   watch: {
-    selectedCategoryId(newCategoryId) {
-      this.filterProgramsByCategory(newCategoryId)
+
+    programListAll(newList) {
+      this.programListFilteredByCategory = newList.slice()
     },
-    
+
+    selectedCategoryId(newCategoryId) {
+      console.log("You have selected category: " + this.selectedCategoryId + "(" + newCategoryId + ")");
+      this.filterProgramsByCategory(newCategoryId);
+    },
+
     selectedProgramId(newProgramId) {
-      console.log("You have selected program: " + newProgramId)
-    }    
-  }
+      console.log("You have selected program: " + this.selectedProgramId + "(" + newProgramId + ")");
+    },
 
+    programListFilteredByCategory(newArray) {
+      // A new category has been chosen: automatically select the first program in the category
+      if (newArray.length > 0) {
+        this.selectedProgramId = String(newArray[0].id);
+      }
+    },
+  },
 };
-
 </script>
 
 <style>
-    canvas {
-          background-color: rgb(255, 255, 255);
-          border: 1px solid black;
-          width: 320px;
-          height: 320px;
-    }
+canvas {
+  background-color: rgb(255, 255, 255);
+  border: 1px solid black;
+  width: 320px;
+  height: 320px;
+}
 
-    @media only screen and (min-width: 800px) {
-      canvas {
-          width: 800px;
-          height: 800px;
-      }
-    }
-
-
-
+@media only screen and (min-width: 800px) {
+  canvas {
+    width: 800px;
+    height: 800px;
+  }
+}
 </style>
