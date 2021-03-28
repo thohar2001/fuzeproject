35494e6d37ebf089b3046f6e5edf662917e79d60
Thomas Haraldsson <thohar2001@skola.goteborg.se>
2021-03-28 20:33:44 +0200

    Added logo
    Added logo

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <div><img src="..\assets\FUZELOGGA.png" alt="logo">HELLOOO</div> 
+    <div><img src="..\assets\Logga.png" alt="logo" id="logo"></div> 
     <router-link to="/">Trafikmeddelanden</router-link> |
     <router-link to="/PodListing">PodListing</router-link> |
     <router-link to="/musictimeline">Musik timeline</router-link>
@@ -15,6 +15,14 @@
   position: relative;
   padding: 0px;
   width: 100%;
+}
+
+#logo {
+  width: 10%;
+  height: auto;
+  position: absolute;
+  left: 20px;
+  top: 20px;
   
 }
 </style>
