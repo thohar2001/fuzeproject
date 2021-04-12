3f627d7dad14305f8b3f1acc4a70d16e9f3959fd
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-04-12 12:37:33 +0200

    Update data.js
    added Jsdoc to methods

--- src/lib/data.js
+++ src/lib/data.js
@@ -108,6 +108,9 @@ export async function fetchJson(url) {
   return response.json();
 }
 
+/**
+ * Perform a fetch() operation to SR open API and retrieve all channel ID:s
+ */
 export async function getAllChannelIds() {
   const endpoint = `${BASE_URL}/channels?format=json&size=500`;
   const response = await fetchJson(endpoint);
@@ -189,7 +192,9 @@ export function getDaysInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
 
-
+/**
+ * Perform a fetch() operation to SR open API and retrieve all Program
+ */
 export async function getAllPrograms() {
   const endpoint = `${BASE_URL}/programs/index?format=json&filter=program.haspod&filtervalue=true&pagination=false`;
 
@@ -201,6 +206,9 @@ export async function getAllPrograms() {
   return result;
 }
 
+/**
+ * Perform a fetch() operation to SR open API and retrieve all Podcasts
+ */
 export async function getAllPods(programid, durationMin, durationMax) {
   const endpoint = `${BASE_URL}/podfiles?programid=${programid}&pagination=false&format=json`;
   const response = await fetchJson(endpoint);
