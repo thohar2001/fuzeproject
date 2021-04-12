429e0cb54106b8e23b4fe25eba8c3bc831c18d3d
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-04-12 09:49:02 +0200

    Refactor code for Reversed barchart y-axis location.

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,10 +1,10 @@
 <template>
   <h1 id="month" :style="'color: ' + colorMonth">
-      {{ new Date(this.startdate).getMonthName() }}
+    {{ new Date(this.startdate).getMonthName() }}
   </h1>
   <!-- <div :id="uniqueID" :class="reversed ? 'reversed' : 'regular'"></div> -->
   <template v-if="showLoading" style="color: white">
-    <div class="spinner-border" role="status" style="color: white;">
+    <div class="spinner-border" role="status" style="color: white">
       <span class="sr-only"></span>
     </div>
   </template>
@@ -32,7 +32,7 @@ export default {
   },
   data() {
     return {
-      barColors: ['#FA3C4C', '#FFC300', '#44BEC7', '#0084FF'],
+      barColors: ["#FA3C4C", "#FFC300", "#44BEC7", "#0084FF"],
       uniqueID: "chart" + this.id,
       colorMonth: null,
       showLoading: true,
@@ -43,47 +43,8 @@ export default {
           data: [],
         },
       ],
-      chartOptions: {
-        chart: {
-          type: "bar",
-          height: 350,
-          foreColor: '#FFFFFF',
-          toolbar: {
-            show: false
-          }
-        },
-        plotOptions: {
-          bar: {
-            borderRadius: 4,
-            horizontal: true,
-          },
-        },
-        dataLabels: {
-          enabled: true,
-        },
-        xaxis: {
-          labels: {
-            show: false
-          },
-          axisTicks: {
-            show: false,
-          },
-          categories: [],
-          axisBorder: {
-            show: false
-          },
-        },
-        yaxis: {
-          reversed: this.reversed,
-          axisTicks: {
-            show: false,
-          },
-        },
-        grid: {
-          show: false,
-        },
-      },
 
+      chartOptions: getApexChartsSettingsObject(this.reversed, [], []),
       //ApexCharts 3.0 model ends
     };
   },
@@ -101,7 +62,7 @@ export default {
     },
     barcolor: {
       type: Number,
-    }
+    },
   },
   created() {
     Date.prototype.monthNames = [
@@ -125,17 +86,16 @@ export default {
   },
 
   async mounted() {
-
     // Hide all charts while they are loading
     this.$refs.app.style.display = "none";
 
     // The chart chooses color depending on the value of the bar chart (barcolor prop)
-    // 4 different colors to choose from 
-    let theColor = []
-    
+    // 4 different colors to choose from
+    let theColor = [];
+
     // set a color for this month
-    this.colorMonth = this.barColors[this.barcolor % 4]
-    theColor.push(this.barColors[this.barcolor % 4])  
+    this.colorMonth = this.barColors[this.barcolor % 4];
+    theColor.push(this.barColors[this.barcolor % 4]);
 
     let date = new Date(this.startdate);
     const toplist = await getTopFiveArtists(
@@ -162,82 +122,113 @@ export default {
     // Add play amounts to chart.
     this.series[0]["data"] = newSeriesDataValues;
     // Add tracks to chart. P.S. The chart will be refreshed once this object is set.
-    this.chartOptions = {
-      chart: {
-        type: "bar",
-        height: 350,
-        toolbar: {
-          show: false
-        }
+
+    this.chartOptions = getApexChartsSettingsObject(this.reversed, theColor, newXaxisCategoriesValues);
+  },
+};
+
+function getApexChartsSettingsObject(
+  reversedChartBoolean,
+  barColorArray,
+  newXaxisCategoriesValuesArray
+) {
+  let settingsObject = {
+    chart: {
+      type: "bar",
+      foreColor: "#FFFFFF",
+      height: 350,
+      toolbar: {
+        show: false,
       },
-      plotOptions: {
-        bar: {
-          borderRadius: 4,
-          horizontal: true,
-        },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
       },
-      dataLabels: {
-        enabled: true,
-        dropShadow: {
+    },
+    dataLabels: {
+      enabled: true,
+      dropShadow: {
         enabled: true,
         left: 2,
         top: 2,
-        opacity: 0.5
-        }
+        opacity: 0.5,
       },
-        fill: {
-          colors: theColor,
-          type: 'gradient',
-          gradient: {
-          shade: 'dark',
-          type: "horizontal",
-          shadeIntensity: 0.5,
-          gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 50, 100],
-          colorStops: []
-          }
-        },
-      xaxis: {
-        categories: newXaxisCategoriesValues,
-        title: {
-          text: "Spelningar/mån",
-          offsetX: 0,
-          offsetY: 0,
-          style: {
-              color: undefined,
-              fontSize: '12px',
-              fontFamily: 'Helvetica, Arial, sans-serif',
-              fontWeight: 600,
-              cssClass: 'apexcharts-xaxis-title',
-          },
+    },
+    fill: {
+      colors: barColorArray,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "horizontal",
+        shadeIntensity: 0.5,
+        gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 100],
+        colorStops: [],
+      },
+    },
+    xaxis: {
+      labels: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      categories: newXaxisCategoriesValuesArray,
+      title: {
+        text: "Spelningar/mån",
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          color: undefined,
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
+          cssClass: "apexcharts-xaxis-title",
         },
       },
+    },
+    grid: {
+      show: false,
+    },
+  };
 
-      // theme: {
-      //   palette: ['palette1', 'palette2', 'palette3']
-      // },
+  if (reversedChartBoolean) {
+    settingsObject.yaxis = {
+      reversed: reversedChartBoolean,
+      opposite: reversedChartBoolean,
+      axisTicks: {
+        show: false,
+      },
     };
-  },
-};
+  }
+
+  return settingsObject;
+}
 </script>
 
+
+
 <style>
-
 #month {
-    text-align: center;
-    font-weight: 1000;
-    font-size: 2em;
-    margin-top: 1em;
-    color: barColor;
-    width: 100px;
-    margin-left: 50%;
+  text-align: center;
+  font-weight: 1000;
+  font-size: 2em;
+  margin-top: 1em;
+  color: barColor;
+  width: 100px;
+  margin-left: 50%;
 
-    display: flex; /* or inline-flex */
-    align-items: center; 
-    justify-content: center;
+  display: flex; /* or inline-flex */
+  align-items: center;
+  justify-content: center;
 }
 .reversed {
   padding-right: 50%;
@@ -255,7 +246,4 @@ export default {
     padding-left: 0%;
   }
 }
-
-
-
 </style>
